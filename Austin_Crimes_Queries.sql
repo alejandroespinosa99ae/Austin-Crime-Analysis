5215bba00966be912dd5fecb1a4c3d0bcb990ee6
alejandroespinosa99ae <153782195+alejandroespinosa99ae@users.noreply.github.com>
2025-02-25 20:09:56 -0600

    Update Austin_Crimes_Queries.sql
    Updated and added more queries to the file

--- Austin_Crimes_Queries.sql
+++ Austin_Crimes_Queries.sql
@@ -1,11 +1,12 @@
-# How many rows are there in the dataset? There are 116,672 rows
-SELECT count(*) as total_number_rows 
-FROM `bigquery-public-data.austin_crime.crime`;
+# There are 116,672 rows
+
+#SELECT count(*) as total_number_rows 
+#FROM `bigquery-public-data.austin_crime.crime`;
 
 
-# How many unique crimes are in the dataset? There are 50 unique crimes
+ How many unique crimes are in the dataset? There are 50 unique crimes
 select 
-distinct description
+  distinct(description)
 from `bigquery-public-data.austin_crime.crime`;
 
 
@@ -32,8 +33,7 @@ select
 from cte_crimes;
 
 
-
-# From what year(s) is the data from? 2014-2016
+# What are the distinct years in the dataset?
 
 SELECT distinct(year)
 FROM `bigquery-public-data.austin_crime.crime`;
@@ -41,10 +41,55 @@ FROM `bigquery-public-data.austin_crime.crime`;
 
 # How many crimes occurred each year?
 
+SELECT 
+  year,
+  count(primary_type) as crimes_per_year
+FROM `bigquery-public-data.austin_crime.crime`
+group by year;
 
 
+# What is the most common primary_type crime?
+SELECT 
+  primary_type,
+  count(primary_type) as count_of_crime
+FROM `bigquery-public-data.austin_crime.crime`
+group by primary_type
+order by count_of_crime desc
+limit 1;
 
+# Count the number of missing addresses
+select
+  count(*) as total_num_rows,
+  count(address) as num_addresses,
+  count(*) - count(address) as missing_addresses
+from `bigquery-public-data.austin_crime.crime`;
 
+# What are the earliest and latest timestamps?
+SELECT 
+  min(timestamp) as `Min`,
+  max(timestamp) as `Max`
+FROM `bigquery-public-data.austin_crime.crime`;
 
+# How many crimes occured in each police district?
+SELECT
+  district,
+  count(primary_type) as district_crime_count
+FROM `bigquery-public-data.austin_crime.crime`
+group by district
+order by district_crime_count desc;
 
+# What is the distribution of crimes by clearance?
+SELECT 
+  primary_type,
+  count(clearance_status)
+FROM `bigquery-public-data.austin_crime.crime`
+group by primary_type;
+
+# What are the top 5 locations with the most crime?
+SELECT 
+  location,
+  count(primary_type)
+FROM `bigquery-public-data.austin_crime.crime`
+group by location
+order by location desc;
 
